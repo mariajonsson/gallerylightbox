bac4ddb20fd1232ac519f75dbbc5096f716ebdaa
Maria Jonsson <maria.jonsson@tam-arkiv.se>
2015-12-18 15:48:18 +0100

    gallery html structure

---
+++ gallery.php
@@ -0,0 +1,17 @@
+<div class='gallery'>
+  <div class='gallery-current'>
+  	<span class='gallery-currentimg'>
+  		<a href='' class='prev-link'>&lt;</a>
+  		<a href='#' class='lightbox'><img /></a>
+  		<a href='' class='next-link'>&gt;</a>
+  	</span>
+  <p class='image-caption'>Caption</p>
+  </div>
+  <div class='gallery-all'>
+  <a href='pic1.jpg'><img src='pic1_thumb' alt='image' title='Image caption' /></a>
+  <a href='pic2.jpg'><img src='pic2_thumb' alt='image' title='Image caption' /></a>
+  <a href='pic3.jpg'><img src='pic3_thumb' alt='image' title='Image caption' /></a>
+  <a href='pic4.jpg'><img src='pic4_thumb' alt='image' title='Image caption' /></a>
+  <a href='pic5.jpg'><img src='pic5_thumb' alt='image' title='Image caption' /></a>
+   </div>
+</div>
